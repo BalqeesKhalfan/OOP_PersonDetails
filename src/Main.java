5189191d6e3770d8448557e6f096cffdf88caa2d
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-31 10:06:09 +0400

    Accessing on PersonDetails class
    to display the full name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name:");
+        String lastName = scanner.nextLine();
+        PersonDetails personDetails=new PersonDetails(firstName,lastName);
+        System.out.println("Full Name is :"+personDetails.getFullName());
     }
 }
